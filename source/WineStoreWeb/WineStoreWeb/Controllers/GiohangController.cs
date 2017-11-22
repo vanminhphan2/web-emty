7584739a6ca18322fd7f69c0cae6b463512ab8f6
DESKTOP-DC0QH00\vanmi <vanminhphan2@gmail.com>
2017-11-22 21:44:12 +0700

    source code web
    commit source code web

---
+++ source/WineStoreWeb/WineStoreWeb/Controllers/GiohangController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WineStoreWeb.Models;
+
+namespace WineStoreWeb.Controllers
+{
+    public class GiohangController : Controller
+    {
+        dbQLBANRUOUDataContext data = new dbQLBANRUOUDataContext();
+        // GET: Giohang
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> Lstgiohang = Session["Giohang"] as List<Giohang>;
+            if (Lstgiohang == null)
+            {
+                Lstgiohang = new List<Giohang>();
+                Session["Giohang"] = Lstgiohang;
+            }
+            return Lstgiohang;
+        }
+        public ActionResult ThemGiohang(int iMaRuou, string strUrl)
+        {
+            List<Giohang> Lstgiohang = Laygiohang();
+            Giohang sanpham = Lstgiohang.Find(n => n.iMaRuou == iMaRuou);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(iMaRuou);
+                Lstgiohang.Add(sanpham);
+                return Redirect(strUrl);
+
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strUrl);
+            }
+        }
+        private int Tongsoluong()
+        {
+            int iTongsoluong = 0;
+            List<Giohang> Lstgiohang = Session["Giohang"] as List<Giohang>;
+            if (Lstgiohang!=null){
+                iTongsoluong = Lstgiohang.Sum(n => n.iSoluong);
+
+            }
+            return iTongsoluong;
+        }
+        private double Tongtien()
+        {
+            double iTongtien = 0;
+            List<Giohang> Lstgiohang = Session["Giohang"] as List<Giohang>;
+            if (Lstgiohang != null)
+            {
+                iTongtien = Lstgiohang.Sum(n => n.dThanhtien);
+
+            }
+            return iTongtien;
+        }
+        public ActionResult Giohang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "WineStore");
+            }
+            ViewBag.Tongsoluong = Tongsoluong();
+            ViewBag.Tongtien = Tongtien();
+            return View(lstGiohang);
+        }
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = Tongsoluong();
+            ViewBag.Tongtien = Tongtien();
+            return PartialView();
+        }
+        public ActionResult XoaGiohang(int iMaSP)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanphan = lstGiohang.SingleOrDefault(n => n.iMaRuou == iMaSP);
+            if (sanphan != null)
+            {
+                lstGiohang.RemoveAll(n => n.iMaRuou == iMaSP);
+                return RedirectToAction("Giohang");
+            }
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "WineStore");
+            }
+            return RedirectToAction("Giohang");
+        }
+        public ActionResult CapnhatGiohang(int iMaSP, FormCollection f)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanphan = lstGiohang.SingleOrDefault(n => n.iMaRuou == iMaSP);
+            if (sanphan != null)
+            {
+                sanphan.iSoluong = int.Parse(f["txtSoluong"].ToString());
+            }
+            
+            return RedirectToAction("Giohang");
+        }
+        public ActionResult XoatatcaGiohang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "WineStore");
+          
+        }
+        [HttpGet]
+        public ActionResult Dathang()
+        {
+            if(Session["Taikhoan"] == null || Session["Taikhoan"].ToString() == "")
+            {
+                    return RedirectToAction("Dangnhap", "Nguoidung");
+             }
+                if (Session["Giohang"] == null)
+                {
+                    return RedirectToAction("Index", "WineStore");
+                }
+            List<Giohang> lstGiohang = Laygiohang();
+            
+            ViewBag.Tongsoluong = Tongsoluong();
+            ViewBag.Tongtien = Tongtien();
+            return View(lstGiohang);
+        }
+        public ActionResult Dathang(FormCollection collection)
+        {
+            DONDATHANG ddh = new DONDATHANG();
+            KHACHHANG kh = (KHACHHANG)Session["Taikhoan"];
+            List<Giohang> gh = Laygiohang();
+            ddh.MaKH = kh.MaKH;
+            ddh.Ngaydat = DateTime.Now;
+            var ngaygiao = String.Format("{0:MM/dd/yyyy}", collection["Ngaygiao"]);
+            ddh.Ngaygiao= DateTime.Parse(ngaygiao);
+            ddh.Tinhtranggiaohang = false;
+            ddh.Dathanhtoan = false;
+            data.DONDATHANGs.InsertOnSubmit(ddh);
+            data.SubmitChanges();
+            foreach(var item in gh)
+            {
+                CHITIETDONTHANG ctdh = new CHITIETDONTHANG();
+                ctdh.MaDonHang = ddh.MaDonHang;
+                ctdh.MaRuou = item.iMaRuou;
+                ctdh.Soluong = item.iSoluong;
+                ctdh.Dongia = (decimal)item.dGiaban;
+                data.CHITIETDONTHANGs.InsertOnSubmit(ctdh);
+            }
+            data.SubmitChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("Xacnhandonhang","Giohang");
+        }
+        public ActionResult Xacnhandonhang()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ source/WineStoreWeb/WineStoreWeb/Controllers/WineStoreController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WineStoreWeb.Models;
+using PagedList;
+using PagedList.Mvc;
+namespace WineStoreWeb.Controllers
+{
+    public class WineStoreController : Controller
+    {
+        // GET: WineStore
+        dbQLBANRUOUDataContext data = new dbQLBANRUOUDataContext();
+        private List<RUOU> Layruoumoi(int count)
+        {
+            var ruoungoai = data.RUOUs.OrderByDescending(a => a.Vip == 1).Take(5).ToList();
+            return ruoungoai;
+            //return data.RUOUs.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var ruoumoi = Layruoumoi(3);
+            return View(ruoumoi);
+        }
+        private List<RUOU> Laydsruou(int count)
+        {
+            return data.RUOUs.OrderByDescending(a => a.MaRuou).Take(count).ToList();
+        }
+        public ActionResult NuocNK()
+        {
+            var ruoungoai = from lr in data.NUOCNHAPKHAUs where lr.MaNNK <4 || lr.MaNNK >7 select lr; 
+            return PartialView(ruoungoai);
+        }
+        public ActionResult Ruounoi()
+        {
+            var ruounoi = from lr in data.NUOCNHAPKHAUs where lr.MaNNK > 4 && lr.MaNNK <8 select lr;
+            return PartialView(ruounoi);
+        }
+        public ActionResult LayruoutheonuocNK(int id, int ? page)
+        {//tạo biến qui định trên mỗi trang
+            int pageSize = 6;
+            //tạo biến số trang
+            int pageNum = (page ?? 1);
+            var ruou = from r in data.RUOUs where r.MaNNK ==id select r;
+            return View(ruou.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult Details(int id)
+        {
+            var ruou = from r in data.RUOUs where r.MaRuou== id select r;
+            return View(ruou.Single());
+        }
+        public ActionResult Ruoumy()
+        {
+            var ruoungoai = data.RUOUs.OrderByDescending(a => a.MaNNK == 1).Take(3).ToList();
+            return PartialView(ruoungoai);
+        }
+        public ActionResult RuouVN()
+        {
+            var ruouvn = data.RUOUs.OrderByDescending(a => a.MaNNK == 5).Take(3).ToList();
+            return PartialView(ruouvn);
+        }
+        public ActionResult Ruounoibat1()
+        {
+            var ruouvn = data.RUOUs.OrderByDescending(a => a.MaNNK ==2 ).Take(1).ToList();
+            return PartialView(ruouvn);
+        }
+        public ActionResult Ruounoibat2()
+        {
+            var ruouvn = data.RUOUs.OrderByDescending(a => a.MaNNK == 3).Take(1).ToList();
+            return PartialView(ruouvn);
+        }
+        public ActionResult RuouVNnoibat()
+        {
+            var ruouvn = data.RUOUs.OrderByDescending(a => a.MaNNK == 5).Take(4).ToList();
+            return PartialView(ruouvn);
+        }
+        [HttpPost]
+        public ActionResult Timkiem(FormCollection f, int? page)
+        {
+            string keyword = f["txtTimkiem"].ToString();
+           // ViewBag.Tukhoa = keyword;
+            var dstimkiem = data.RUOUs.Where(n => n.TenRuou.Contains(keyword)).ToList();
+            //tạo biến qui định trên mỗi trang
+            int pageSize = 6;
+            //tạo biến số trang
+            int pageNum = (page ?? 1);
+            if(dstimkiem.Count==0)
+            {
+                ViewBag.Thongbao = "khong tim thay san pham nao";
+                return View(data.RUOUs.OrderBy(n=>n.TenRuou).ToPagedList(pageNum,pageSize));
+            }
+
+            ViewBag.Thongbao = "Da tim thấy : "+ dstimkiem.Count+ "kết quả";
+            return View(dstimkiem.OrderBy(n=>n.TenRuou).ToPagedList(pageNum, pageSize));
+        }
+        [HttpGet]
+        public ActionResult Timkiem(int? page, string keyword)
+        {
+            ViewBag.Tukhoa = keyword;
+            var dstimkiem = data.RUOUs.Where(n => n.TenRuou.Contains(keyword)).ToList();
+            //tạo biến qui định trên mỗi trang
+            int pageSize = 6;
+            //tạo biến số trang
+            int pageNum = (page ?? 1);
+            if (dstimkiem.Count == 0)
+            {
+                ViewBag.Thongbao = "khong tim thay san pham nao";
+                return View(data.RUOUs.OrderBy(n => n.TenRuou).ToPagedList(pageNum, pageSize));
+            }
+
+            ViewBag.Thongbao = "Da tim thấy : " + dstimkiem.Count + "kết quả";
+            return View(dstimkiem.OrderBy(n => n.TenRuou).ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult timkiemruou(FormCollection f,string key)
+        {
+            string keyword = f["txtTimkiem"].ToString();
+            // ViewBag.Tukhoa = keyword;
+            var dstimkiem = data.RUOUs.Where(n => n.TenRuou.Contains(keyword)).ToList();
+            //tạo biến qui định trên mỗi trang
+     
+            if (dstimkiem.Count == 0)
+            {
+                ViewBag.Thongbao = "khong tim thay san pham nao";
+                return View(data.RUOUs.OrderBy(n => n.TenRuou));
+            }
+
+            ViewBag.Thongbao = "Da tim thấy : " + dstimkiem.Count + "kết quả";
+
+            return View(data.RUOUs.OrderBy(n => n.TenRuou));
+        }
+        public ActionResult search(FormCollection f, int? page)
+        {//tạo biến qui định trên mỗi trang
+            string key =f["searchtext"].ToString();
+            int pageSize = 6;
+            //tạo biến số trang
+            int pageNum = (page ?? 1);
+            var ruou = data.RUOUs.Where(n => n.TenRuou.Contains(key)).ToList();
+            return View(ruou.ToPagedList(pageNum, pageSize));
+        }
+    }
+}
